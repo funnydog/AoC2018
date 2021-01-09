3836b093354610a68d13488e17880e5b9b6c5c24
funnydog <funnydog@users.noreply.github.com>
2021-01-09 10:28:07 +0100

    Add the emacs customizations for the project

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (setq flycheck-clang-include-path
+		       (list
+			(expand-file-name "~/projects/AoC/2018/lib")
+			)))))
+ (c++-mode . ((flycheck-clang-language-standard . "c++17"))))
